80558980086be00d58328897be4a1d720be1fce5
Megan Galinis <megan.galinis@gmail.com>
2023-04-10 11:36:05 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    }
+    saturdayFun()
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.` 
+    }
+    mondayWork()
+
+    function wrapAdjective (style="*") {
+        return function(stringOne="special"){
+          return `You are ${style}${stringOne}${style}!`
+        }
+      }
+
